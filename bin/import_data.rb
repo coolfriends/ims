819fbe98363e06ab7f10b138908bbcf50c4a9b11
bougyman <bougyman@voidlinux.eu>
2017-03-27 04:37:52 +0000

    Fixes another date field with bad data (mr_lm_date)

--- bin/import_data.rb
+++ bin/import_data.rb
@@ -11,6 +11,10 @@ tables = Dir['data/*.dbf'] if tables.empty?
 
 def fix_hash(h)
   %w(me_timetor
+     mr_lm_date
+     ao_infsta2
+     ap_lm_date
+     ao_infend2
      ij_last_mo
      or_loctime
      sd_last_mo).each do |key|
@@ -23,7 +27,7 @@ tables.each do |table|
   dbf = DBF::Table.new(table)
   rows = 1
   dbf.each do |row|
-    puts "Loading row #{rows} of table #{table_name}"
+    puts "Loading row #{rows} of table #{table_name}" if rows % 100 == 0
     rows += 1
     next if row.nil?
     row_hash = Hash[row.attributes.map { |k, v| [k.downcase, v] }]
